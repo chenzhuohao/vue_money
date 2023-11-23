d2b69ad72027fa50ba8b8cf82de8288c5e3b8e9e
cyc <2938287734@qq.com>
2023-11-23 11:01:23 +0800

    授权展示页全部逻辑

--- src/views/ShowCredit.vue
+++ src/views/ShowCredit.vue
@@ -5,9 +5,9 @@
     <div class="box box-one">
       <div class="top">
         <span>您的预计放款额度</span>
-        <span>有效期至：2024-02-30</span>
+        <span>有效期至：{{ validityPeriod }}</span>
       </div>
-      <div class="center">￥2000000</div>
+      <div class="center">{{ isExpired ? '额度失效' : '￥' + quota }}</div>
       <div class="bottom">仅表示预计授信额度，具体放款以实际情况为准</div>
     </div>
     <div class="box box-two">
@@ -34,12 +34,59 @@
     <div class="btn-box">
       <el-button type="primary" class="btn">返回</el-button>
     </div>
-    <a href="#" class="jump">额度太低？立即联系我们</a>
+    <a href="#" class="jump" @click="connect">额度太低？立即联系我们</a>
+    <div class="phone-dialog" v-if="showPhoneDialog" @click="closePhoneDialog">
+      <div class="content" v-if="isMobile">
+        <div class="phone">{{ phone }}</div>
+        <div class="button-box">
+          <el-button type="text" class="btn" @click="call">拨打</el-button>
+          <el-button type="text" class="btn" @click="closePhoneDialog">取消</el-button>
+        </div>
+      </div>
+      <div class="content">
+        <img src="https://lh3.googleusercontent.com/ogw/AKPQZvyzOeOESuO8k8SWmwh9dz39rW5pCqm3-6ZcYS_P=s32-c-mo" class="img" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import NavBar from '@/components/NavBar/NavBar.vue'
+import { ref, computed  } from 'vue'
+
+const validityPeriod = ref('202-02-30')
+const quota = ref(2000000)
+const phone = ref('400-000-0000')
+const showPhoneDialog = ref(false)
+// 移动端还是PC端
+const isMobile = ref(false)
+
+// 计算是否过期
+const isExpired = computed(() => {
+  const currentTime = new Date();
+  const expirationDate = new Date(validityPeriod.value);
+  return currentTime > expirationDate;
+});
+
+// 联系我们
+const connect = () => {
+  // 判断是PC端还是移动端
+  if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
+    showPhoneDialog.value = true
+  } else {
+    window.location.href = 'tel:400-000-0000'
+  }
+}
+
+// 关闭弹窗
+const closePhoneDialog = () => {
+  showPhoneDialog.value = false
+}
+
+// 拨打
+const call = () => {
+  window.location.href = 'tel:' + phone.value
+}
 </script>
 
 <style lang="scss" scoped>
@@ -142,6 +189,68 @@ import NavBar from '@/components/NavBar/NavBar.vue'
       border-radius: 1rem;
       background-color: #0581fe;
       font-family: PingFangSC-regular;
+    }
+  }
+
+  .phone-dialog {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+
+    .content {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      background-color: #ffffff;
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      border-radius: 13px 13px 13px 13px;
+      width: 16.875rem;
+
+      .img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        overflow: hidden;
+      }
+      
+      .phone {
+        width: 100%;
+        height: 2.75rem;
+        border-radius: 1rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #030303;
+        font-size: 1.125rem;
+        font-family: PingFangSC-regular;
+        padding: 1.8rem 0;
+      }
+
+      .button-box {
+        display: flex;
+        width: 100%;
+        border-top: #9ea6b7 1px solid;
+
+        .btn {
+          color: #0076FF;
+          font-size: 1.05rem;
+          font-family: PingFangSC-regular;
+          height: 2.5rem;
+          width: 50%;
+          text-align: center;
+          &:first-child {
+            border-right: #9ea6b7 1px solid;
+          }
+        }
+      }
     }
   }
 }
